ba15e3014a4431d5b6e6c68a0fa56fd718195d76
Biryukov <biryukov.mm@gmail.com>
2018-02-23 15:09:30 +0200

    Добавил шаблон под список категорий в меню базовой страницы. (base.html, head.css) Мелкие ошибки исправлены.

--- blog/models.py
+++ blog/models.py
@@ -20,12 +20,16 @@ class Pseudo(models.Model):
     slug = models.SlugField(max_length=60, unique=True, default='')
     create_date = models.DateField(auto_created=True)
 
+    def get_related_posts(self):
+        query = self.post_set.all().order_by('-pub_date')
+        return query
+
     def __str__(self):
         return self.name
 
 
 class Tag(models.Model):
-    name = models.CharField(max_length=20)
+    name = models.CharField(max_length=30)
     slug = models.SlugField(max_length=60, unique=True, default='')
 
     def get_related_posts(self):
@@ -37,9 +41,9 @@ class Tag(models.Model):
 
 
 class Post(models.Model):
-    title = models.CharField(max_length=50)
-    slug = models.SlugField(max_length=60, unique=True, default='')
-    text = models.TextField(max_length=30000)
+    title = models.CharField(max_length=100)
+    slug = models.SlugField(max_length=130, unique=True, default='')
+    text = models.TextField(max_length=60000)
     create_date = models.DateTimeField(blank=True, auto_now_add=True)
     pub_date = models.DateTimeField(blank=True, null=True)
     category = models.ForeignKey(Category, on_delete=models.PROTECT)
