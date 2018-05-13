a45c6703f4971862641fc62c6966b8de08be6c42
Biryukov-m <biryukov.mm@gmail.com>
2018-05-13 11:44:46 +0300

    Поправлен скрипт github_getter на текущий репозиторий. Но коммиты из старого репозитория в api не отображаются. В запрос передаются 20 коммитов. Добавлена проверка на слово "Обновле(кхе-кхе)ние" для того чтобы отображались только коммиты с крупными обновлениями.
    Исправлены проблемы с футером и боковыми отступами.
    Поправлено отображение формы редактирования поста, и логина в админку.
    
    Добавлен файл requirements.txt

--- blog/lib/github_getter.py
+++ blog/lib/github_getter.py
@@ -3,17 +3,18 @@ import datetime
 
 
 def get_commits():
-    url = 'https://api.github.com/repos/biryukov-m/django-media/commits'
+    url = 'https://api.github.com/repos/biryukov-m/с-media/commits?page=1&per_page=20'
     try:
         r = get(url)
         dic = r.json()
         output = []
-        for d in dic[:10]:
+        for d in dic:
             date = d['commit']['committer']['date']
             date = datetime.datetime.strptime(date, "%Y-%m-%dT%H:%M:%SZ")
             message = d['commit']['message']
             html_url = d['html_url']
-            output.append([date, message, html_url])
+            if "Обновление" in message:
+                output.append([date, message, html_url])
         return output
     except:
         return None
